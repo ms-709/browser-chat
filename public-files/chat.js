12c57d3e46ca29235e08b46c5ea9bf314fc1a75c
ms-709 <msutton0709@gmail.com>
2026-01-29 14:58:04 -0600

    Added custom name colors and a chat message preview

--- public-files/chat.js
+++ public-files/chat.js
@@ -4,8 +4,20 @@ var socket = io.connect();
 
 var chatWindow = document.getElementById('chat-window'),
     chatBox = document.getElementById('chat-box'),
-    sendButton = document.getElementById('send-button');
-    usernameBox = document.getElementById('username-box');
+    sendButton = document.getElementById('send-button'),
+    usernameBox = document.getElementById('username-box'),
+
+    sampleMessageName = document.getElementById('sample-message-name'),
+    nameColorPicker = document.getElementById('name-color-picker');
+
+//---------- STYLE VARS ----------//
+var messageNameColor = nameColorPicker.value,
+    messageNameBorderColor = `color-mix(in srgb, ${messageNameColor} 90%, white 10% )`;
+
+//---------- INITIALIZE ----------//
+sampleMessageName.innerHTML = '<p>' + usernameBox.value + '</p>';// necessary to reflect attributes persisting with cookies
+sampleMessageName.style.backgroundColor = messageNameColor; // ^
+sampleMessageName.style.borderColor = messageNameBorderColor; // ^
 
 //---------- EMIT EVENTS ----------//
 
@@ -24,12 +36,14 @@ sendButton.addEventListener('click', function(){
     {
         socket.emit('sendMessage', {
             username: usernameBox.value,
-            chatMessage: chatBox.value
+            chatMessage: chatBox.value,
+            namecolor: messageNameColor,
+            bordercolor: messageNameBorderColor
         });
 
         // create the local message
         chatWindow.innerHTML += '<div class="chat-message-outgoing">' + 
-                                    '<div class="message-name">' + 
+                                    '<div class="message-name"  style="background-color: ' + messageNameColor + '; border-color: ' + messageNameBorderColor + ';">' + 
                                         '<h2>' + usernameBox.value + '</h2>' + 
                                     '</div>' + 
                                 '<div class = "message-body">' + 
@@ -39,9 +53,11 @@ sendButton.addEventListener('click', function(){
         chatBox.value = ""; // clear the message box on send
     }
 
-
-
     chatWindow.scrollTop = chatWindow.scrollHeight; // *this scrolls the thing to show new messages at bottom
+});
+
+usernameBox.addEventListener('input', function(){
+    sampleMessageName.innerHTML = '<p>' + usernameBox.value + '</p>';
 });
 
 chatBox.addEventListener('keydown', function(event){
@@ -51,11 +67,21 @@ chatBox.addEventListener('keydown', function(event){
     }
 });
 
+nameColorPicker.addEventListener('change', function(){
+    console.log(`${messageNameColor}`);
+
+    messageNameColor = nameColorPicker.value; // update js color variable
+    messageNameBorderColor = `color-mix(in srgb, ${messageNameColor} 90%, white 10% )`;
+
+    sampleMessageName.style.backgroundColor = messageNameColor; // update preview color
+    sampleMessageName.style.borderColor = messageNameBorderColor; // update preview border color
+});
+
 //---------- LISTEN EVENTS ----------//
 
 socket.on('sendMessage', function(data){
     chatWindow.innerHTML += '<div class="chat-message-incoming">' + 
-                                '<div class="message-name">' + 
+                                '<div class="message-name"  style="background-color: ' + data.namecolor + '; border-color: ' + data.bordercolor + ';">' +  
                                     '<h2>' + data.username + '</h2>' + 
                                 '</div>' + 
                             '<div class = "message-body">' + 
